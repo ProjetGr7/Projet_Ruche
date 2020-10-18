c95ac4dc60043bd54e226387ecfdd3acb96c7223
Kzel <baoyining12@gmail.com>
2020-10-18 17:05:44 +0200

    Update main.cpp
    modification plus lisible

--- main.cpp
+++ main.cpp
@@ -1,62 +1,57 @@
 #include "mbed.h"
 #include "DS1820.h"
 #include "DHT.h"
-  
-  
- //Code pour DS18B20
-  Serial      pc(USBTX, USBRX);
-  DHT sensor(D3, DHT22);
+
+  //serial du module wisol sigfox et PC
+  Serial pc(USBTX, USBRX);
   Serial sigfox(D1,D0);
 
+  //Capteur de temperature/humidite et sonde
+  DHT dht22(D3, DHT22);
+  DS1820      ds1820(D4); 
+
+  //verification par LED
   DigitalOut  led(LED1);
-  DS1820      ds1820(D4);  // substitute D4 with actual mbed pin name connected to 1-wire bus
-  float       temp = 0;
-  int         result = 0;
+ 
+
   
   int main()
   {
-    int error = 0;
-    int h = 0, c = 0;
+    int erreur = 0;
+    int humidite = 0, tempDHT22 = 0; //temperature et humidite de DHT22
+    float       temp = 0; //temperature du sonde
+    int         resultat = 0; //verifier le statut de ds18b20
+      
+      pc.printf("\r\n--Commencer--\r\n");
+      erreur = dht22.readData(); 
     
-      pc.printf("\r\n--Starting--\r\n");
-         error = sensor.readData(); 
-        if (error == 0)                            
-        {
-            c   = sensor.ReadTemperature(CELCIUS);
-            h   = sensor.ReadHumidity();
-            
-            
-            //pc.printf("temperature %d\n", c);
-            //pc.printf("humidite %d\n,", h);
-        } 
-        else
-        {
-            printf("Error: %d\n", error);
-        } 
-      if (ds1820.begin()) {
+      if (ds1820.begin() && erreur == 0) {
           while (1) {
-              ds1820.startConversion();   // start temperature conversion from analog to digital
-              //ThisThread::sleep_for(1000);// let DS1820 complete the temperature conversion
+              ds1820.startConversion();     // commencer a converser
+              //ThisThread::sleep_for(1000); // laisser DS1820 complete la conversion
               wait(10);
-              result = ds1820.read(temp); // read temperature from DS1820 and perform cyclic redundancy check (CRC)
-              switch (result) {
-                  case 0:                 // no errors -> 'temp' contains the value of measured temperature
+              resultat = ds1820.read(temp); // lire temperature par ds1820 et lancer cyclic redundancy check (CRC)
+              tempDHT22 = dht22.ReadTemperature(CELCIUS); // lire temperature par DHT22
+              humidite= dht22.ReadHumidity(); // lire humidite par DHT22
+              switch (resultat) {
+                  case 0:                 // pas d'erreur -> 'temp' comporte valeur de temperature
                       //pc.printf("temp = %3.1f%cC\r\n", temp, 176);
-                      sigfox.printf("AT$SF=%02x%02x%02x\r\n",c,h,(int)temp);
+                      sigfox.printf("AT$SF=%02x%02x%02x\r\n",c,h,(int)temp); //envoyer le message a backend sigfox
+
                     break;
   
-                  case 1:                 // no sensor present -> 'temp' is not updated
-                      pc.printf("no sensor present\n\r");
+                  case 1:                 // pas de capteur present -> 'temp' n'est pas mis a jour
+                      pc.printf("pas de capteur present\n\r");
                       break;
   
-                  case 2:                 // CRC error -> 'temp' is not updated
-                      pc.printf("CRC error\r\n");
+                  case 2:                 // erreur de CRC  -> 'temp' n'est pas mis a jour
+                      pc.printf("erreur de CRC\r\n");
               }
   
-              led = !led;
+              led = !led; // verification par LED
           }
       }
       else
-         pc.printf("No DS1820 sensor found!\r\n");
+         pc.printf("erreur de connection ds18b20 ou DHT22!\r\n");
         
   }
